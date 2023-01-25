66f4623ed29f1e6be97681ca345e50d38c958e35
Henrique Rodrigues <henrique_krf@outlook.com>
2023-01-25 08:34:07 -0300

    Icon configurado / Header finalizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { Container, Title, ButtonMenu, } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function Header({ title }){
+  const navigation = useNavigation();
   return(
     <Container>
+      <ButtonMenu onPress={ () =>navigation.openDrawer()}>
+        <Icon name="menu" size={35} color="#121212"/>
+      </ButtonMenu>
       { title && (
         <Title>{title}</Title>
       )}
